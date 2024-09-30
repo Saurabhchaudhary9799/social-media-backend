cb574277cd4bea301c804ac706b7fd065b9e5a86
Saurabhchaudhary9799 <saurbhbailara@gmail.com>
2024-09-30 18:40:28 +0530

    cors origin updated

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://sky-social.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 200,
